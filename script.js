c18efa10903a50e2941b8db62e9503283e50f345
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-25 11:01:33 +0500

    finished js

--- script.js
+++ script.js
@@ -23,105 +23,105 @@ let next = document.querySelector(".next");
 let draw = document.querySelector(".draw");
 
 X.addEventListener("click", () => {
-    user1 = "X"
-    user2 = "O"
-    btns.style.cssText = "display: none"
+  user1 = "X"
+  user2 = "O"
+  btns.style.cssText = "display: none"
 });
 
 O.addEventListener("click", () => {
-    user1 = "O"
-    user2 = "X"
-    btns.style.cssText = "display: none"
+  user1 = "O"
+  user2 = "X"
+  btns.style.cssText = "display: none"
 });
 
 let winningPosition = [
-    [one, two, three],
-    [four, five, six],
-    [seven, eight, nine],
-    [one, four, seven],
-    [two, five, eight],
-    [three, six, nine],
-    [one, five, nine],
-    [three, five, seven]
+  [one, two, three],
+  [four, five, six],
+  [seven, eight, nine],
+  [one, four, seven],
+  [two, five, eight],
+  [three, six, nine],
+  [one, five, nine],
+  [three, five, seven]
 ];
 
 function win() {
-    winningPosition.forEach((item) => {
-        for (let i = 0; i < item.length; i++) {
-            if (item[0].textContent == "X" && item[1].textContent == "X" && item[2].textContent == "X") {
-                win1.style.cssText = "display: block;";
-                item[0].style.cssText = "color: red;";
-                item[1].style.cssText = "color: red;";
-                item[2].style.cssText = "color: red;";
-                draw.style.cssText = "display: none;";
-                next.style.cssText = "display: block;";
-                noneFunctionX();
-            } else if (item[0].textContent == "O" && item[1].textContent == "O" && item[2].textContent == "O") {
-                win2.style.cssText = "display: block;";
-                item[0].style.cssText = "color: red;";
-                item[1].style.cssText = "color: red;";
-                item[2].style.cssText = "color: red;";
-                draw.style.cssText = "display: none;";
-                next.style.cssText = "display: block;";
-                noneFunctionO();
-            }
-        }
-    });
+  winningPosition.forEach((item) => {
+    for (let i = 0; i < item.length; i++) {
+      if (item[0].textContent == "X" && item[1].textContent == "X" && item[2].textContent == "X") {
+        win1.style.cssText = "display: block;";
+        item[0].style.cssText = "color: red;";
+        item[1].style.cssText = "color: red;";
+        item[2].style.cssText = "color: red;";
+        draw.style.cssText = "display: none;";
+        next.style.cssText = "display: block;";
+        noneFunctionX();
+      } else if (item[0].textContent == "O" && item[1].textContent == "O" && item[2].textContent == "O") {
+        win2.style.cssText = "display: block;";
+        item[0].style.cssText = "color: red;";
+        item[1].style.cssText = "color: red;";
+        item[2].style.cssText = "color: red;";
+        draw.style.cssText = "display: none;";
+        next.style.cssText = "display: block;";
+        noneFunctionO();
+      }
+    }
+  });
 }
 
-
 function noneFunctionX() {
-    katak.forEach((item) => {
-        if (item.textContent == "" || item.textContent == "O") {
-            item.textContent = none;
-        }
-    });
+  katak.forEach((item) => {
+    if (item.textContent == "" || item.textContent == "O") {
+      item.textContent = none;
+    }
+  });
 }
 
 function noneFunctionO() {
-    katak.forEach((item) => {
-        if (item.textContent == "" || item.textContent == "X") {
-            item.textContent = none;
-        }
-    });
+  katak.forEach((item) => {
+    if (item.textContent == "" || item.textContent == "X") {
+      item.textContent = none;
+    }
+  });
 }
 
 let round = 0;
 katak.forEach((item) => {
-    item.addEventListener("click", (e) => {
-        if (item.textContent == "") {
-            if (count % 2 == 1) {
-                item.textContent = user1
-            } else if (count % 2 == 0) {
-                item.textContent = user2
-            }
-            count++;
-            win()
-            if (count == 10) {
-                tie.style.cssText = "display: block;";
-                next.style.cssText = "display: block";
-                draw.style.cssText = "display: none;";
-            }
-        }
-    });
+  item.addEventListener("click", (e) => {
+    if (item.textContent == "") {
+      if (count % 2 == 1) {
+        item.textContent = user1
+      } else if (count % 2 == 0) {
+        item.textContent = user2
+      }
+      count++;
+      win()
+      if (count == 10) {
+        tie.style.cssText = "display: block;";
+        next.style.cssText = "display: block";
+        draw.style.cssText = "display: none;";
+      }
+    }
+  });
 });
 
-
 function restart() {
-    katak.forEach((item) => {
-        item.textContent = ""
-    });
-    round = 0;
-    count = 1;
-    btns.style.cssText = "display: block";
-    draw.style.cssText = "display: block;";
-    next.style.cssText = "display: none;";
-    win1.style.cssText = "display: none;";
-    win2.style.cssText = "display: none;";
-    tie.style.cssText = "display: none;";
-    katak.forEach((item) => {
-        item.style.cssText = "color: white;"
-    })
+  katak.forEach((item) => {
+    item.textContent = ""
+  });
+  round = 0;
+  count = 1;
+  btns.style.cssText = "display: block";
+  draw.style.cssText = "display: block;";
+  next.style.cssText = "display: none;";
+  win1.style.cssText = "display: none;";
+  win2.style.cssText = "display: none;";
+  tie.style.cssText = "display: none;";
+  katak.forEach((item) => {
+    item.style.cssText = "color: white;"
+  })
 }
 
-
+next.addEventListener('click', () => {
+  restart();
+})
